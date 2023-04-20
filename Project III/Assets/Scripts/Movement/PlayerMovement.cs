0485b886da6ae3124986a46d13640389a588acfd
jeonmaxx <jazuendorf@stud.mediadesign.de>
2023-04-20 17:47:59 +0200

    DialogueSystem Added

--- Project III/Assets/Scripts/Movement/PlayerMovement.cs
+++ Project III/Assets/Scripts/Movement/PlayerMovement.cs
@@ -5,21 +5,30 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Vector2 movement;
+    public Vector2 movement;
     private Rigidbody2D rb;
     [SerializeField]
     private float speed = 5;
     private Animator animator;
+    DialogueManager dialogueManager;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        dialogueManager = FindObjectOfType<DialogueManager>();
     }
 
     private void OnMovement(InputValue value)
     {
-        movement = value.Get<Vector2>();
+        if (dialogueManager.isActive == false)
+        {
+            movement = value.Get<Vector2>();
+        }
+        else
+        {
+            movement = new Vector2(0, 0);
+        }
 
         if (movement.x != 0 || movement.y != 0)
         {
