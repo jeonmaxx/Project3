4ce043b98d80fd50fd76683c6a5d0aa01eae3d6c
jeonmaxx <jazuendorf@stud.mediadesign.de>
2023-07-28 16:04:09 +0200

    ui updated

--- Brewbarians/Assets/!Scripts/Inventory/MainInventory/OpenInventory.cs
+++ Brewbarians/Assets/!Scripts/Inventory/MainInventory/OpenInventory.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.InputSystem;
-
+using UnityEditor.Animations;
 
 public class OpenInventory : MonoBehaviour
 {
@@ -15,14 +15,21 @@ public class OpenInventory : MonoBehaviour
     public Vector3 newToolBarPos;
     [ShowOnly] public Windows windows;
 
+    public Sprite slotImageUp;
+    public Sprite slotImageDown;
+
     public RectTransform mainInventory;
     public RectTransform map;
     public RectTransform recipe;
+
+    private Color color;
 
     public void Start()
     {
         toolBarPos = new Vector3(toolBar.GetComponent<RectTransform>().anchoredPosition.x, toolBar.GetComponent<RectTransform>().anchoredPosition.y, 0);
         windows = Windows.Inventory;
+
+        color = toolBar.GetComponent<Image>().color;
     }
 
     public void Update()
@@ -31,25 +38,36 @@ public class OpenInventory : MonoBehaviour
         {
             inventoryBox.GetComponent<Canvas>().enabled = false;
             toolBar.GetComponent<RectTransform>().anchoredPosition = new Vector3(toolBarPos.x, toolBarPos.y, 0);
+            for(int i = 0; i < toolBar.transform.childCount; i++)
+            {
+                toolBar.transform.GetChild(i).GetComponent<Image>().sprite = slotImageDown;
+            }
+
+            toolBar.GetComponent<Image>().color = new Color(color.r, color.g, color.b, 100);
         }
         else
         {
             inventoryBox.GetComponent<Canvas>().enabled = true;
 
-            if (windows == Windows.Inventory)
-                toolBar.GetComponent<RectTransform>().anchoredPosition = new Vector3(newToolBarPos.x, newToolBarPos.y, 0);
+            toolBar.GetComponent<RectTransform>().anchoredPosition = new Vector3(newToolBarPos.x, newToolBarPos.y, 0);
+            for (int i = 0; i < toolBar.transform.childCount; i++)
+            {
+                toolBar.transform.GetChild(i).GetComponent<Image>().sprite = slotImageUp;
+            }
+
+            toolBar.GetComponent<Image>().color = new Color(color.r, color.g, color.b, 0);
         }
 
         switch (windows)
         {
             case Windows.Inventory:
-                mainInventory.SetSiblingIndex(2);
+                mainInventory.SetSiblingIndex(6);
                 break;
             //case Windows.Map:
-            //    map.SetSiblingIndex(1);
+            //    map.SetSiblingIndex(6);
             //    break;
             case Windows.Recipe:
-                recipe.SetSiblingIndex(2);
+                recipe.SetSiblingIndex(6);
                 break;
         }
     }
